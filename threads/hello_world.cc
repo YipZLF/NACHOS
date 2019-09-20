dcbb9f61caca8ace17739eaebbc4b02d0024a93e
Yip Zilingfeng <yipzlf@pku.edu.cn>
2019-09-20 19:12:41 +0800

    This is the code of NachOS after adding Hello_world.

---
+++ threads/hello_world.cc
@@ -0,0 +1,14 @@
+/*
+ * @Description: In User S     
+ * ettings Edit
+ * @Author: your name
+ * @Date: 2019-09-18 13:43:34
+ * @LastEditTime: 2019-09-18 14:08:26
+ * @LastEditors: Please set LastEditors
+ */
+#include"hello_world.h"
+#include"utility.h"
+
+void print_hello(){
+    printf(helloworld);
+}
---
+++ threads/hello_world.h
@@ -0,0 +1,8 @@
+/*
+ * @Description: Test Makefile. Print Hello world when initialized
+ * @Author: YipZLF
+ * @Date: 2019-09-18 13:48:02
+ */
+
+static char* helloworld="Hello world!\n";
+void print_hello();
